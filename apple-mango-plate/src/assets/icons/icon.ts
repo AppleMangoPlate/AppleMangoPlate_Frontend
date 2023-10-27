705845d81170055a75b4cacb8e579b262eba0202
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-10-27 14:33:26 +0900

    Feat : Header v2

---
+++ apple-mango-plate/src/assets/icons/icon.ts
@@ -0,0 +1,9 @@
+import { FaRegUser } from "react-icons/fa";
+
+type IconType = React.ElementType;
+
+const icons: Record<string, IconType> = {
+  userIcons: FaRegUser,
+};
+
+export default icons;
--- apple-mango-plate/src/components/Layout/header.tsx
+++ apple-mango-plate/src/components/Layout/header.tsx
@@ -1,13 +1,24 @@
+import Link from "next/link";
 import React from "react";
+import icons from "@/assets/icons/icon";
+import { FaRegUser } from "react-icons/fa";
+
+const UserIcon = icons.userIcons;
 
 const Header = () => {
   return (
-    <div className="sticky top-0 w-full h-16 flex flex-row justify-between bg-primary-yellow">
-      <div className="text-[black]">
-        <p>hi</p>
-      </div>
-      <div>
-        <p>hi</p>
+    <div className="sticky top-0 w-full h-16 bg-primary-yellow">
+      <div className="flex h-16 items-center justify-between  text-[black] mx-10">
+        <div>
+          <Link href="/">
+            <p>이미지 들어갈곳</p>
+          </Link>
+        </div>
+        <div>
+          <button>
+            <UserIcon color="black" size={24} />
+          </button>
+        </div>
       </div>
     </div>
   );
