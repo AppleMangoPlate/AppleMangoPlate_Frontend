e0b07b93e17dd9942c6e2d4cb07c109e92c5af75
obvoso <autumninmoon@gmail.com>
2023-11-08 13:06:32 +0900

    fix: unused module 주석처리

--- apple-mango-plate/src/hooks/useAuth.ts
+++ apple-mango-plate/src/hooks/useAuth.ts
@@ -1,4 +1,4 @@
-import { verityJwtToken } from "@/utils/auth";
+//import { verityJwtToken } from "@/utils/auth";
 import { JwtPayload } from "jsonwebtoken";
 import React from "react";
 import Cookies from "universal-cookie";
@@ -9,8 +9,8 @@ export function useAuth() {
   const getVerifiedtoken = async () => {
     const cookies = new Cookies();
     const token = cookies.get("token") ?? null;
-    const verifiedToken = await verityJwtToken(token);
-    setAuth(verifiedToken);
+    //const verifiedToken = await verityJwtToken(token);
+    //setAuth(verifiedToken);
   };
   React.useEffect(() => {
     getVerifiedtoken();
