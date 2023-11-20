9d7c1a1d0bcbe1e0316c141b404d9ed6158e4961
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-11-20 20:31:35 +0900

    Feat: Mypage

--- apple-mango-plate/src/pages/mypage/index.tsx
+++ apple-mango-plate/src/pages/mypage/index.tsx
@@ -1,5 +1,56 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { myPageState } from "@/atoms/myPageIdx";
+import Layout from "@/components/Layout/layout";
+
 const MyPage = () => {
-  return <p>my Page</p>;
+  const myPageData = useRecoilValue(myPageState);
+  const setMyPageData = useSetRecoilState(myPageState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const emailData = localStorage.getItem("email");
+        const accessToken = localStorage.getItem("accessToken");
+
+        const response = await axios.get(`/user/mypage/${emailData}`, {
+          headers: {
+            access_token: `${accessToken}`,
+          },
+        });
+        const { id, email, nickName, role, phoneNumber, profileImage } =
+          response.data;
+
+        setMyPageData({
+          ...myPageData,
+          id,
+          email,
+          nickName,
+          role,
+          phoneNumber,
+          profileImage,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [myPageData, setMyPageData]);
+
+  return (
+    <Layout>
+      <div>
+        <p>ID: {myPageData.id}</p>
+        <p>Email: {myPageData.email}</p>
+        <p>Nickname: {myPageData.nickName}</p>
+        <p>Role: {myPageData.role}</p>
+        <p>Phone Number: {myPageData.phoneNumber}</p>
+        <p>Profile Image: {myPageData.profileImage}</p>
+      </div>
+    </Layout>
+  );
 };
 
 export default MyPage;
---
+++ apple-mango-plate/src/types/mypage.ts
@@ -0,0 +1,8 @@
+export interface MypageDTO {
+  id: number;
+  email: string;
+  nickName: string;
+  role: string;
+  phoneNumber: string;
+  profileImage: string;
+}
