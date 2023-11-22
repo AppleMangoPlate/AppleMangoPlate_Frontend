4583145940f25310437cf4384af5e5dbcc625369
obvoso <autumninmoon@gmail.com>
2023-11-22 14:38:38 +0900

    refactor: kakao map sdk module 사용

--- apple-mango-plate/src/components/store/PlaceMap.tsx
+++ apple-mango-plate/src/components/store/PlaceMap.tsx
@@ -1,4 +1,7 @@
-import { useEffect } from "react";
+import Script from "next/script";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`;
 
 interface Props {
   x: number;
@@ -6,44 +9,15 @@ interface Props {
 }
 
 export default function PlaceMap({ x, y }: Props) {
-  useEffect(() => {
-    const mapScript = document.createElement("script");
-    mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`;
-    document.head.appendChild(mapScript);
-
-    const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById("map");
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(y, x),
-          level: 3,
-          marker: true,
-        };
-        const map = new window.kakao.maps.Map(mapContainer, mapOption);
-
-        // Create a marker
-        const markerPosition = new window.kakao.maps.LatLng(y, x);
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition,
-        });
-
-        // Add the marker
-        marker.setMap(map);
-      });
-    };
-    mapScript.addEventListener("load", onLoadKakaoMap);
-
-    // Clean up on unmount
-    return () => {
-      mapScript.removeEventListener("load", onLoadKakaoMap);
-    };
-  }, [x, y]); // Add x and y to the dependency array
-
   return (
-    <div
-      id="map"
-      className="mt-16 flex w-[450px] h-[250px] md:w-[650px] md:h-[300px] border-2 border-black"
-    />
+    <>
+      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
+      <Map
+        center={{ lat: y, lng: x }}
+        className="mt-16 flex w-[450px] h-[250px] md:w-[650px] md:h-[300px] border-2 border-black"
+      >
+        <MapMarker position={{ lat: y, lng: x }} />
+      </Map>
+    </>
   );
 }
