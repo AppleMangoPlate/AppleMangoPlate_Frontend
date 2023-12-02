8c568c67ad6d0ee777d6c9b4b0ff1929c5bd01c5
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-12-02 21:08:24 +0900

    Design : Mypage header + button 만들기

--- apple-mango-plate/src/pages/mypage/index.tsx
+++ apple-mango-plate/src/pages/mypage/index.tsx
@@ -30,12 +30,34 @@ const MyPage = () => {
   return (
     <Layout>
       <div>
-        <p>ID: {myPageData.id}</p>
-        <p>Email: {myPageData.email}</p>
-        <p>Nickname: {myPageData.nickName}</p>
-        <p>Role: {myPageData.role}</p>
-        <p>Phone Number: {myPageData.phoneNumber}</p>
-        <p>Profile Image: {myPageData.profileImage}</p>
+        <div className="w-full h-16 bg-mypage-header z-50">
+          <div className="flex h-16 items-center justify-between text-3xl text-white ml-20">
+            <p>My Page</p>
+          </div>
+        </div>
+        <div className="w-full h-16 border-b-[3px] border-mypage-header">
+          <div className="flex flex-row h-16 items-center text-xl text-primary-orange ml-20">
+            <button className="bg-white border-[2px] border-primary-orange rounded-3xl w-24 py-1 mr-5">
+              내정보
+            </button>
+            <button className="bg-white border-[2px] border-primary-orange rounded-3xl w-24 py-1 mr-5">
+              찜
+            </button>
+            <button className="bg-white border-[2px] border-primary-orange rounded-3xl w-24 py-1">
+              리뷰모음
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center justify-center h-screen">
+          <div>
+            <p>ID: {myPageData.id}</p>
+            <p>Email: {myPageData.email}</p>
+            <p>Nickname: {myPageData.nickName}</p>
+            <p>Role: {myPageData.role}</p>
+            <p>Phone Number: {myPageData.phoneNumber}</p>
+            <p>Profile Image: {myPageData.profileImage}</p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
